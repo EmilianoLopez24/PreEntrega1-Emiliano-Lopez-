bf549753ee1a8769f4d17cae74c4a3b5a4fd326d
EmilianoLopez <emiliano2042004@gmail.com>
2023-08-01 19:51:39 -0300

    Segunda entrega

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-// Calculadora de IMC
+// Calculadora de Indice de Masa Corporal
 
-// 1 Preguntar al usuario su nombre, altura y peso
+// Creamos un array para almacenar data del usuario
+let usersData = [];
 
-const nombreUsuario = prompt("Cual es tu nombre?");
-alert("Bienvenido " + nombreUsuario);
-let usuarioAltura = prompt(nombreUsuario + " Cuál es tu altura en metros?");
-let usuarioPeso = prompt(nombreUsuario + " Cuál es tu peso en KG?");
+// Funcion para calcular IMC
+function calcularimc(peso, altura) {
+  if (isNaN(peso) || isNaN(altura) || altura <= 0) {
+    return "Porfavor introduzca una altura y peso válidos";
+  }
 
-// 2 Calcular su IMC
+  let imc = peso / (altura * altura);
+  imc = Math.round(imc * 10) / 10;
 
-let usuarioIMC = usuarioPeso / (usuarioAltura * usuarioAltura);
-console.log(usuarioIMC);
-alert(`${nombreUsuario}, tu Indice de Masa Corporal es ${usuarioIMC}`);
+  usersData.push(imc);
 
-// 3 En base a su IMC, Compararlo con el mío y alertar ese resultado
-
-let imcEmiliano = 71 / (1.75 * 1.75);
-console.log(imcEmiliano);
-
-if (usuarioIMC > imcEmiliano) {
-  alert(
-    `${nombreUsuario} , tu IMC es mayor al de Emiliano (${imcEmiliano}), el creador de este programa`
-  );
-} else {
-  alert(
-    `Emiliano, el creador del programa tiene un Indice de masa Corporal mayor al tuyo. Su IMC es ${imcEmiliano}. Deberiamos decirle que deje de comer tanto! `
-  );
+  return imc;
 }
+console.log(usersData);
+
+// Ejemplo de uso:
+let user1imc = calcularimc(65, 1.75);
+let user2imc = calcularimc(70, 1.8);
+
+console.log(user1imc);
+console.log(user2imc);
+
+// Ejemplo de uso numero 2: Recibimos data de personas proveniente de algun API en forma de objetos y queremos calcular su IMC
+
+const data1 = {
+  nombre: "Emiliano Lopez",
+  edad: "19",
+  altura: "1.75",
+  peso: "70",
+};
+
+const data2 = {
+  nombre: "Tadeo Ramos",
+  edad: "26",
+  altura: "1.84",
+  peso: "74",
+};
+
+const data3 = {
+  nombre: "Gabriel Rivero",
+  edad: "47",
+  altura: "1.66",
+  peso: "80",
+};
+
+let data1IMC = calcularimc(data1.peso, data1.altura);
+console.log(`El IMC de esta persona es ${data1IMC}`);
+
+let data2IMC = calcularimc(data2.peso, data2.altura);
+console.log(`El IMC de esta persona es ${data2IMC}`);
+
+let data3IMC = calcularimc(data3.peso, data3.altura);
+console.log(`El IMC de esta persona es ${data3IMC}`);
